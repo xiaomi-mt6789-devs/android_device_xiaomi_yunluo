3c5f549cb15388ee69a3028983b963aa00b89c7c
SamarV-121 <samarvispute121@pm.me>
2022-05-05 21:11:22 +0100

    yunluo: Build wifi services from source
    * Rename libwifi-hal to libwifi-hal-mtk prevent conflict with aosp's
      libwifi-hal (credits: SamarV-121)
    
    * Import wifi service makefile and modify it to use libwifi-hal-mtk
      (credits: bengris32)
    
    Co-Authored-By: bengris32 <bengris32@protonmail.ch>
    Change-Id: I86f357083dbbb911cb6ff4512665b26e1bfcfaec

--- device.mk
+++ device.mk
@@ -224,22 +224,9 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-	android.system.wifi.keystore@1.0.vendor:64 \
-	android.hardware.wifi.hostapd@1.0.vendor:64 \
-	android.hardware.wifi.hostapd@1.1.vendor:64 \
-	android.hardware.wifi.hostapd@1.2.vendor:64 \
-	android.hardware.wifi.hostapd@1.3.vendor:64 \
-	android.hardware.wifi.supplicant@1.0.vendor:64 \
-	android.hardware.wifi.supplicant@1.1.vendor:64 \
-	android.hardware.wifi.supplicant@1.2.vendor:64 \
-	android.hardware.wifi.supplicant@1.3.vendor:64 \
-	android.hardware.wifi.supplicant@1.4.vendor:64 \
-	android.hardware.wifi@1.0.vendor:64 \
-	android.hardware.wifi@1.1.vendor:64 \
-	android.hardware.wifi@1.2.vendor:64 \
-	android.hardware.wifi@1.3.vendor:64 \
-	android.hardware.wifi@1.4.vendor:64 \
-	android.hardware.wifi@1.5.vendor:64 \
+    android.hardware.wifi@1.0-service-lazy.yunluo \
+    wpa_supplicant \
+    hostapd \
 	libkeystore-wifi-hidl:64 \
     libkeystore-engine-wifi-hidl:64
 
