46fc3952a9ce5bd3d0b5c0ec2228bd6b8a4aad1b
Woomymy <woomy@woomy.be>
2023-02-27 15:31:40 +0100

    yunluo: Add partitions configuration
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I9e08db2236376b1132aa3bd8a7abf19aa70ba48c

--- device.mk
+++ device.mk
@@ -8,5 +8,8 @@
 PRODUCT_SOONG_NAMESPACES += \
 	$(LOCAL_PATH)
 
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Inherit our proprietary vendor
 $(call inherit-product, vendor/xiaomi/yunluo/yunluo-vendor.mk)
