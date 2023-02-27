8f536c66f1cee8e06ff18e6331d75e2483d3b3bc
Woomymy <woomy@woomy.be>
2023-02-27 12:05:05 +0100

    yunluo: Define architecture and bootloader flags
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: Ib38ecb1687d2e4716f9b39e2e53e2847a62a4a1d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,30 @@
 
 DEVICE_PATH := device/xiaomi/yunluo
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a76
+TARGET_CPU_VARIANT_RUNTIME := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a55
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
+
+# OTA
+TARGET_OTA_ASSERT_DEVICE := yunluo
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := yunluo
+TARGET_NO_BOOTLOADER := true
+
+# Plaform
+TARGET_BOARD_PLATFORM := mt6789 
+
 # Inherit the proprietary version
 include vendor/xiaomi/yunluo/BoardConfigVendor.mk
