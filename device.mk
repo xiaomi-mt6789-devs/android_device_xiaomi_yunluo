8f7211e7887bac7b842e40edf57246edca25551d
Woomymy <woomy@woomy.be>
2023-02-27 19:36:08 +0100

    yunluo: Add A/B OTA configuration
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: If5e2f40b6bf55ffa7a62cc67ff3a0b1e8de6d5b3

--- device.mk
+++ device.mk
@@ -8,6 +8,36 @@
 PRODUCT_SOONG_NAMESPACES += \
 	$(LOCAL_PATH)
 
+# A/B
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS := \
+	boot \
+	vendor_boot \
+	system \
+	vendor \
+	product \
+	vbmeta \
+	vbmeta_system \
+	vbmeta_vendor
+
+PRODUCT_PACKAGES += \
+	update_engine \
+	update_verifier \
+	otapreopt_script \
+	checkpoint_gc
+
+AB_OTA_POSTINSTALL_CONFIG += \
+	RUN_POSTINSTALL_system=true \
+	POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+	FILESYSTEM_TYPE_system=ext4 \
+	POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
