803596382fa44ecfd6698d19c7929e3b3e6df581
Woomymy <woomy@woomy.be>
2023-02-27 15:34:34 +0100

    yunluo: device: Set shipping API Level
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I63444728089f8f40b333a5229fd003b34463b2af

--- device.mk
+++ device.mk
@@ -14,5 +14,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
+# Shipping API Level
+PRODUCT_SHIPPING_API_LEVEL := 31
+
 # Inherit our proprietary vendor
 $(call inherit-product, vendor/xiaomi/yunluo/yunluo-vendor.mk)
