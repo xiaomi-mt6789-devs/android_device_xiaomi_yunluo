884a77e685820cfb91d02e5d255d7a12decf4694
Woomymy <woomy@woomy.be>
2023-03-02 20:44:31 +0100

    yunluo: Import stock media configs
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I0018f022f1e92f94d88af84603d3bf70c7888b02

--- device.mk
+++ device.mk
@@ -69,6 +69,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
 	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
+# Media
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
