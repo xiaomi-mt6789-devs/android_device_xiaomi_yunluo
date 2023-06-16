8660d8399eb43126595398e20b9e9e5c8defed02
Woomymy <woomy@woomy.be>
2023-06-16 21:12:14 +0200

    yunluo: extract-files: Remove useless patches for memtrack/power blobs
    * Those are now built from source
    
    Signed-off-by: Woomymy <woomy@woomy.be>

--- extract-files.sh
+++ extract-files.sh
@@ -86,12 +86,6 @@ function blob_fixup() {
         vendor/lib64/mt6789/libmtkcam_stdutils.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
-        vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service)
-            "${PATCHELF}" --replace-needed "android.hardware.power-V2-ndk_platform.so" "android.hardware.power-V2-ndk.so" "${2}"
-            ;;
-        vendor/bin/hw/android.hardware.memtrack-service.mediatek)
-            "${PATCHELF}" --replace-needed "android.hardware.memtrack-V1-ndk_platform.so" "android.hardware.memtrack-V1-ndk.so" "${2}"
-            ;;
     esac
 }
 
