2a3a450302cd79f512a73f327347f5dcc8f8a0be
Woomymy <woomy@woomy.be>
2023-07-27 20:42:28 +0000

    yunluo: Install charger res images to vendor
    Change-Id: I9e7776bc8ec532b000c5934b45adaeb62aa72543
    Signed-off-by: Woomymy <woomy@woomy.be>

--- device.mk
+++ device.mk
@@ -154,7 +154,8 @@ PRODUCT_PACKAGES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health-service.mt6789 \
-    android.hardware.health-service.mt6789-recovery
+    android.hardware.health-service.mt6789-recovery \
+    charger_res_images_vendor
 
 # HIDL
 PRODUCT_PACKAGES += \
