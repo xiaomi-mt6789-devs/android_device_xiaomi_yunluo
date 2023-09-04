3b5335865920faaa4ef1029d2d1e270bca749e28
bengris32 <bengris32@protonmail.ch>
2023-09-04 20:04:37 +0100

    yunluo: Switch to common AIDL USB service
    Change-Id: Ie7bc896c51c435ddd7906f02409c3610386e9ea3
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -237,7 +237,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-mediatekv2
+    android.hardware.usb-service.mediatek
 
 # Enable audio accessory support
 $(call soong_config_set,android_hardware_mediatek_usb,audio_accessory_supported,true)
