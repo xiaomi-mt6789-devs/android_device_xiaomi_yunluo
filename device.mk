b08b7a289cad2a84ffd09af18ef82fb488badac3
bengris32 <bengris32@protonmail.ch>
2023-10-05 15:34:45 +0100

    yunluo: Switch Thermal to AIDL
    Change-Id: I89be258d0683e3497639551a76459527d4366afa
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -235,7 +235,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.mediatek
+    android.hardware.thermal-service.mediatek
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
