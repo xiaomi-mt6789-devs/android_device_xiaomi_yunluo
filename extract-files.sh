d09fd8a2ed3bf48b55d679619be86d5cb0e441c0
Woomymy <woomy@woomy.be>
2023-11-12 13:36:18 +0100

    yunluo: Link media.c2 service against v33 `libstagefright_foundation`
    * Fixes crashes and high CPU usage
    
    Change-Id: I60c07d10c2db9335d20c6dbaaaeacc9313aebf06
    Signed-off-by: Woomymy <woomy@woomy.be>

--- extract-files.sh
+++ extract-files.sh
@@ -76,6 +76,7 @@ function blob_fixup() {
             ;;
         vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
             "${PATCHELF}" --replace-needed "libavservices_minijail_vendor.so" "libavservices_minijail.so" "${2}"
+            "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
             ;;
         vendor/lib64/hw/mt6789/vendor.mediatek.hardware.pq@2.15-impl.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
@@ -111,3 +112,5 @@ extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTIO
 "${MY_DIR}/setup-makefiles.sh"
 
 vndk_import "${ANDROID_ROOT}" "libutils" "32" "both" "vndk-sp"
+vndk_import "${ANDROID_ROOT}" "libstagefright_foundation" "33" "both" "vndk-core"
+
