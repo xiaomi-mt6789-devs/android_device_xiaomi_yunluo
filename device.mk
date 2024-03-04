7b8512035e00bd046c5bf2e991078e302fd63edd
bengris32 <bengris32@protonmail.ch>
2024-03-04 21:42:35 +0000

    yunluo: Switch to common MediaTek USB Gadget HAL
    Change-Id: Iba9a20ed667d18ab2f99e3bf5a6f7e353f392484
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -177,7 +177,8 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.mediatek
+    android.hardware.usb-service.mediatek \
+    android.hardware.usb.gadget-service.mediatek
 
 # Enable audio accessory support
 $(call soong_config_set,android_hardware_mediatek_usb,audio_accessory_supported,true)
