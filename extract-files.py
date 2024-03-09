569289eef2ec54e831fbf1d7417277807ca3a397
Michael Bestas <mkbestas@lineageos.org>
2024-03-09 00:01:01 +0000

    yunluo: Add firmware extraction support
    Change-Id: If6cb6334a8f7b2fcc77c8947569eba1b2002190a

--- extract-files.py
+++ extract-files.py
@@ -127,6 +127,7 @@ module = ExtractUtilsModule(
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
     check_elf=True,
+    add_firmware_proprietary_file=True,
 )
 
 if __name__ == "__main__":
